35439337e11b97fc956e558af78e085a69efbecd
haruni24 <haruni20@icloud.com>
2024-12-30 21:48:52 +0900

    :tada: 簡易的なNNを作成

---
+++ Learn/common.py
@@ -0,0 +1,19 @@
+import cshogi
+import numpy as np
+
+def board_to_input(board):
+    input = list()
+    np_board = np.array(board.pieces)
+    for i in range(1,cshogi.WPROM_ROOK):
+        if i==15 or i==16:
+            continue
+        input.append(i==np_board)
+    return np.array(input)
+
+def random_play(board):
+    while True:
+        if board.is_game_over() or len(board.history)>100:
+            break
+        move = np.random.choice(list(board.legal_moves))
+        board.push(move)
+    return 1-board.turn
